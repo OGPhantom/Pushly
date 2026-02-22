12446b9f7e18edfc219efca5f5d378dbae45d9d9
OGPhantom <nikita.storchai@nure.ua>
2026-02-22 16:18:01 +0200

    Initial commit

---
+++ Pushly/PushlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushlyApp.swift
+//  Pushly
+//
+//  Created by Никита Сторчай on 22.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct PushlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
